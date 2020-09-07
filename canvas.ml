0999e480f907773d0c06386b0132b981c4230581
Josh Bell <jbbe@umich.edu>
2020-09-07 13:16:26 -0400

    Canvas tests pass

--- canvas.ml
+++ canvas.ml
@@ -1,11 +1,11 @@
 open Color
 
-
+exception ValueError of string
 class canvas height width = 
   object(self)
-    val pixels_r = Array.make height (Array.make width 0.)
-    val pixels_g = Array.make height (Array.make width 0.)
-    val pixels_b = Array.make height (Array.make width 0.)
+    val pixels_r = Array.make_matrix height width 0.
+    val pixels_g = Array.make_matrix height width 0.
+    val pixels_b = Array.make_matrix height width 0.
     method pixel (x: int) (y:int) =
       {r= pixels_r.(x).(y); g=pixels_g.(x).(y); b=pixels_b.(x).(y)}
     method height =
@@ -13,7 +13,37 @@ class canvas height width =
     method width =
       width
     method write_pixel (x: int) (y:int) (c: color) =
-      pixels_r.(x).(y) <- c.r;
-      pixels_g.(x).(y) <- c.g;
-      pixels_b.(x).(y) <- c.b
+      if x >= height || y >= width then raise (ValueError "Out of bounds")
+      else 
+        pixels_r.(x).(y) <- c.r;
+        pixels_g.(x).(y) <- c.g;
+        pixels_b.(x).(y) <- c.b
+    method to_256 (f: float) : int =
+      let scaled = int_of_float (f *. 255.) in
+      if scaled >= 255 then 255 else if scaled < 0 then 0 else scaled
+    method pixel_to_string (x: int) (y: int) : string =
+      (* Printf.printf "Pixels %f %f" x y; *)
+      let pixel_fmt = format_of_string "%d %d %d " in
+      Printf.sprintf pixel_fmt (self#to_256 pixels_r.(x).(y)) (self#to_256 pixels_g.(x).(y)) (self#to_256 pixels_b.(x).(y)) 
+    method to_ppm : string =
+      (* print_newline (); *)
+      (* let () = pixels_r |> Array.iter (Array.iter print_float) in
+      let () = pixels_g |> Array.iter (Array.iter print_float) in
+      let () = pixels_b |> Array.iter (Array.iter print_float) in *)
+      let header_fmt = format_of_string "P3\n%d %d\n255\n" in
+      let header = Printf.sprintf header_fmt height width in
+      let out = Buffer.create (height * width * 4) in
+      Buffer.add_string out header;
+      for x = 0 to (height-1) do 
+        let overflow_count = ref 0 in
+        for y = 0 to (width-1) do
+          overflow_count := !overflow_count + 2;
+          if !overflow_count >= 60 then Buffer.add_string out "\n";
+          if !overflow_count >= 60 then overflow_count := 0;
+          (* Printf.printf "%s" (Buffer.contents out); *)
+          Buffer.add_string out (self#pixel_to_string x y);
+        done;
+        Buffer.add_string out "\n";
+      done;
+      Buffer.contents out
   end
--- tests/canvas_test.ml
+++ tests/canvas_test.ml
@@ -2,6 +2,24 @@ open OUnit2
 open Canvas
 open Color
 
+let correct_ppm = "P3
+5 3
+255
+255 0 0 0 0 0 0 0 0 
+0 0 0 0 0 0 0 0 0 
+0 0 0 0 127 0 0 0 0 
+0 0 0 0 0 0 0 0 0 
+0 0 0 0 0 255 0 0 0 \n"
+
+let correct_ppm2 = "P3\n10 2\n255\n255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 \n153 255 204 153 255 204 153 255 204 153 255 204 153 \n255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 \n153 255 204 153 255 204 153 255 204 153 255 204 153 \n"
+(* let correct_ppm = "P3
+5 3
+255
+255 0 0 0 0 0 0 0 0 
+0 0 0 0 0 0 0 0 0 
+0 0 0 0 127 0 0 0 0 
+0 0 0 0 0 0 0 0 0 
+0 0 0 0 0 255 0 0 0\n" *)
 let test_create_canvas _ =
   let c = new canvas 10 20 in
   assert_equal 10 c#height;
@@ -11,13 +29,46 @@ let test_create_canvas _ =
 let test_write_pixel _ =
   let c = new canvas 10 20 in
   c#write_pixel 1 2 {r=0.5;g=0.;b=1.};
-  assert_bool "write pixel" (color_equal {r=0.5;g=0.;b=1.} (c#pixel 2 2))
- 
+  assert_bool "write pixel 1" (color_equal {r=0.5;g=0.;b=1.} (c#pixel 1 2));
+  assert_bool "Should be black" (color_equal {r=0.;g=0.;b=0.} (c#pixel 0 0));
+  assert_bool "Should be black" (color_equal {r=0.;g=0.;b=0.} (c#pixel 0 2));
+  assert_bool "Should be black" (color_equal {r=0.;g=0.;b=0.} (c#pixel 2 0))
+
+
+
+let test_canvas_to_ppm _ =
+  let c = new canvas 5 3 
+  and c1 = {r=1.5;g=0.;b=0.}
+  and c2 = {r=0.;g=0.5;b=0.}
+  and c3 = {r=(-0.5);g=0.;b=1.} in
+  c#write_pixel 0 0 c1;
+  c#write_pixel 2 1 c2;
+  c#write_pixel 4 1 c3;
+  let ppm = c#to_ppm in
+  (* Printf.printf "%s" ppm; *)
+  assert_equal correct_ppm ppm
+
+let test_long_ppm_line _ =
+  let c = new canvas 10 2 in
+  let ppm = c#to_ppm in
+  Printf.printf "PPm\n%s" ppm;
+  assert_equal correct_ppm2 ppm
+
+let test_ppm_terminates _ =
+  let c = new canvas 5 3 in
+  let ppm = c#to_ppm in
+  let len = String.length ppm in
+  let last_char = String.get ppm (len - 1) in
+  assert_equal '\n' last_char 
+
 
 let suite =
   "ColorTestList" >::: [
     "test_create_canvas" >:: test_create_canvas;
     "test_write_pixel" >:: test_write_pixel;
+    "test_canvas_to_ppm" >:: test_canvas_to_ppm;
+    (* "test_long_ppm_line" >:: test_long_ppm_line; *)
+    "test_ppm_terminates" >:: test_ppm_terminates;
   ]
 
 let () =
